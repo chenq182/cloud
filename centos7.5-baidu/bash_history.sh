b561d190c553c6ecd11136ce25366ceef98f58f8
Quan Chen <chenq182@sina.com>
2019-05-06 09:49:03 +0800

    2019-05-06 09:47

---
+++ centos7.5-baidu/bash_history.sh
@@ -0,0 +1,17 @@
+mount /dev/vdb1 /mnt
+yum -y upgrade
+yum -y install vim-enhanced
+cp /mnt/docker/docker-ce/centos/vimrc ~/.vimrc
+echo "alias vi='vim'" >> ~/.bashrc
+yum -y install git
+git config --global user.email "chenq182@sina.com"
+git config --global user.name "Quan Chen"
+yum -y install /mnt/docker/docker-ce/centos/libtool-ltdl-2.4.2-22.el7_3.x86_64.rpm 
+yum -y install /mnt/docker/docker-ce/centos/libseccomp-2.3.1-3.el7.x86_64.rpm 
+yum -y install /mnt/docker/docker-ce/centos/container-selinux-2.33-1.git86f33cd.el7.noarch.rpm 
+yum -y install /mnt/docker/docker-ce/centos/docker-ce-17.12.0.ce-1.el7.centos.x86_64.rpm 
+systemctl start docker.service
+systemctl enable docker.service
+cp /mnt/docker/docker-ce/docker-compose /usr/local/bin/
+docker version
+umount /mnt
